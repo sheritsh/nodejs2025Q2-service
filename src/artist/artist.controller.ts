a3bdb70065e49635e9b18ca813678901dd9fea8d
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-06-03 00:36:45 +0300

    feat: implemented artist entity

---
+++ src/artist/artist.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  ParseUUIDPipe,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto, UpdateArtistDto } from '../dto/artist.dto';
+
+@Controller('artist')
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  findAll() {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.artistService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+  ) {
+    return this.artistService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.artistService.remove(id);
+  }
+}
