bb63d48b92758bf6a7a743807f050ac619558432
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-06-16 23:14:42 +0300

    feat: added to project structure logging service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { DatabaseModule } from './database/database.module';
+import { LoggingModule } from './logging/logging.module';
+import { LoggingMiddleware } from './logging/logging.middleware';
 
 @Module({
   imports: [
+    LoggingModule,
     DatabaseModule,
     UserModule,
     ArtistModule,
@@ -16,4 +19,8 @@ import { DatabaseModule } from './database/database.module';
     FavoritesModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
